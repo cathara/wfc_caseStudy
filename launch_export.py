2da022377cab6de0fe02f3700e30959aebaa72bd
Catharactus <117767116+Catharactus@users.noreply.github.com>
2024-04-25 16:31:46 -0400

    message

--- launch_export.py
+++ launch_export.py
@@ -2,9 +2,9 @@ import os
 import subprocess
 
 blender_dir = r"C:\Program Files\Blender Foundation\Blender 4.1\blender.exe"
-blend_path = r"C:\Users\A.azoulay\Desktop\wfc folder\wfc08\wfc\rock.blend"
-python_script = r"C:\Users\A.azoulay\Desktop\wfc folder\wfc08\wfc\export_tile.py"
-output_folder = r"output=C:\Users\A.azoulay\Desktop\wfc folder\wfc08\wfc\Content\wfc\TEST"
+blend_path = r"C:\Users\A.azoulay\Desktop\wfc folder\wfc\rock.blend"
+python_script = r"C:\Users\A.azoulay\Desktop\wfc folder\wfc\export_tile.py"
+output_folder = r"output=C:\Users\A.azoulay\Desktop\wfc folder\wfc\Content\wfc\TEST"
 
 
 command = [
